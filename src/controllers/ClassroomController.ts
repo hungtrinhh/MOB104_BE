7da67049205b3f7a3458d858c1c7b14c9add39b9
owennguyen3162 <nguyentuan3162@gmail.com>
2023-06-03 22:01:31 +0700

    feature/getListClassroom
    feature/getListClassroom

--- src/controllers/ClassroomController.ts
+++ src/controllers/ClassroomController.ts
@@ -41,6 +41,15 @@ class ClassroomController {
     });
   };
 
+  static listAll = async (req: Request, res: Response) => {
+    const queryRunner = AppDataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    let classroom = await queryRunner.manager.query('SELECT tbl_classrooms.name, tbl_classrooms.description, tbl_classrooms.subject, tbl_classrooms.teacher_id,COUNT(tbl_students.id) AS count FROM tbl_classrooms INNER JOIN tbl_class_students  ON tbl_class_students.classroom_id = tbl_classrooms.id INNER JOIN tbl_students ON tbl_class_students.student_id = tbl_students.id WHERE tbl_class_students.semester = 1 GROUP BY tbl_classrooms.id')
+    res.status(200).send({ error: false, data: classroom });
+
+  };
   
 }
 export default ClassroomController;
