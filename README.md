98914701047f49e411416580dd4f9cb18fea0eb9
owennguyen3162 <nguyentuan3162@gmail.com>
2023-06-03 21:53:36 +0700

    config database - file
    config database - file

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# MOB104_BE
-Server môn quản lý dự án
+# EasyEdu API Server
+
+Rest API with Typescript, JWT, MySql, Express, Typeorm
+
+## How to dev
+
+```sh
+npm i 
+npm start
+```
+
+# LICENSE
+
+[MIT](https://github.com/baontq23/jwt-express-mysql-typeorm-typescript-template/blob/master/LICENSE)
---
+++ src/data-source.ts
@@ -0,0 +1,21 @@
+import { DataSource } from 'typeorm';
+import { Teacher } from './entity/Teacher';
+import { Classroom } from './entity/Classroom';
+import { Parent } from './entity/Parent';
+import { Student } from './entity/Student';
+import { ClassStudent } from './entity/ClassStudent';
+import { Feedback } from './entity/Feedback';
+
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: '',
+  database: 'easyedu_db',
+  synchronize: true,
+  logging: false,
+  entities: [Teacher, Classroom, Parent, Student, ClassStudent, Feedback],
+  migrations: ['src/migration/**/*.js'],
+  subscribers: []
+});
