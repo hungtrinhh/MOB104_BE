08aac4ab29fd6df021fd7a3838e4d9f225362304
owennguyen3162 <nguyentuan3162@gmail.com>
2023-06-08 23:24:34 +0700

    getAllInformationParentAndStudent
    feature/classroomStudentController

--- src/controllers/ClassroomStudentController.ts
+++ src/controllers/ClassroomStudentController.ts
@@ -44,6 +44,31 @@ class ClassroomController {
     }
   };
 
-  
+  static getAllInformationParentAndStudent = async (
+    req: Request,
+    res: Response
+  ) => {
+    const id = req.params.id;
+    const idClass = parseInt(req.params.idClass);
+    const queryRunner = AppDataSource.manager;
+    const getAllInformation = await queryRunner.query(
+      'SELECT tbl_students.name AS student_name ,tbl_students.dob AS student_dob ,tbl_students.gender AS student_gender,tbl_class_students.regularScore1 AS regular_score_1,tbl_class_students.regularScore2 AS regular_score_2 ,tbl_class_students.regularScore3 AS regular_score_3,tbl_class_students.midtermScore AS midterm_score,tbl_class_students.finalScore AS final_score, tbl_class_students.semester,tbl_parents.name AS parent_name ,tbl_parents.email AS parent_email,tbl_parents.dob AS parent_dob,tbl_parents.phone AS parent_phone,tbl_parents.fcmToken AS parent_fcmtoken FROM tbl_class_students INNER JOIN tbl_students  ON tbl_class_students.student_id = tbl_students.id INNER JOIN tbl_classrooms  ON tbl_class_students.classroom_id = tbl_classrooms.id INNER JOIN tbl_parents ON tbl_parents.id = tbl_students.parent_id   WHERE tbl_students.id = "' +
+        id +
+        '" AND tbl_classrooms.id = "' +
+        idClass +
+        '"'
+    );
+    if (getAllInformation.length === 0) {
+      res.status(404).send({
+        error: true,
+        code: 404,
+        message: 'Học sinh không tồn tại !'
+      });
+    } else {
+      res.status(200).send({ error: false, data: getAllInformation });
+    }
+  };
+
+ 
 }
 export default ClassroomController;
